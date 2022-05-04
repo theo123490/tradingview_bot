9259693dd1d56f439337584bf4f45a3e7a82fb5e
theo123490 <theo123490@gmail.com>
2022-05-05 01:10:32 +0700

    intial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import tradingview_ta 
+import data_storage
+from tradingview_ta import TA_Handler, Interval, Exchange
+
+def get_summary(stock, exchange="IDX", screener="indonesia", interval="1M"):
+    handler = TA_Handler(
+        symbol=stock,
+        exchange=exchange,
+        screener=screener,
+        interval=interval
+    )
+    return(handler.get_analysis().summary)
+
+idx_list = data_storage.idx_list
+
+buy_list = []
+strong_buy_list = []
+
+for i in idx_list:
+    try: 
+        summary = get_summary(i)
+        if summary['RECOMMENDATION'] == 'BUY' :
+            buy_list.append(i)
+        elif summary['RECOMMENDATION'] == 'STRONG_BUY':
+            strong_buy_list.append(i)
+        print(f"stock:{i} done")
+    except:
+        print(f"stock:{i} fails")
+        pass
+    
+f = open("buy_list.txt", "w")
+f.write(buy_list)
+f.close()
+
+f = open("strong_buy_list.txt", "w")
+f.write(strong_buy_list)
+f.close()
